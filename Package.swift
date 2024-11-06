8e40bcd300acffa9b02693aa81f6b72ae74c9b8c
Stefan Aleksiev <stefan.aleksiev@lab08.com>
2024-11-06 17:16:44 +0200

    main - Update checksum

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(
-            name: "SiteimproveAppAnalytics", url: "https://github.com/Siteimprove/appanalytics-ios-sdk-public/releases/download/1.1.0/SiteimproveAppAnalytics.xcframework.zip", checksum: "351103d777bfaac17042a162208a6874358153a7778b9f8a735c4e74aa109523"
+            name: "SiteimproveAppAnalytics", url: "https://github.com/Siteimprove/appanalytics-ios-sdk-public/releases/download/1.1.0/SiteimproveAppAnalytics.xcframework.zip", checksum: "6b7322e2741b7ee06d377518fe4274a509e3c6048c1b93efa63e1515838b4ce0"
         )
     ]
 )
